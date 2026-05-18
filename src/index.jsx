8d4ad07a114def4256b9788a2971fd056807c991
jasonhorikawa <jasonhorikawa@gmail.com>
2026-05-18 10:47:45 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,32 @@ import ReactDOM from "react-dom/client";
 import CampBook from "./app.jsx";
 import "./styles.css";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  componentDidCatch(error) {
+    this.setState({ error });
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: 20, fontFamily: "Arial", color: "red" }}>
+          <h1>CampBook crashed</h1>
+          <pre>{String(this.state.error)}</pre>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <CampBook />
+  <ErrorBoundary>
+    <CampBook />
+  </ErrorBoundary>
 );
