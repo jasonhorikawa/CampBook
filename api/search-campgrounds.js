00360270caa5de8b7a2411231af3e7478cf4bc57
jasonhorikawa <jasonhorikawa@gmail.com>
2026-05-21 12:16:21 -0700

    Create search-campgrounds.js

---
+++ api/search-campgrounds.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const { query } = req.query;
+
+  if (!query || query.length < 3) {
+    return res.status(200).json({ results: [] });
+  }
+
+  const apiKey = process.env.VITE_RECGOV_API_KEY;
+
+  const response = await fetch(
+    `https://ridb.recreation.gov/api/v1/facilities?query=${encodeURIComponent(query)}&limit=5`,
+    {
+      headers: {
+        apikey: apiKey,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json({
+    results: data.RECDATA || [],
+  });
+}
