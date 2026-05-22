62d6c0c422ff4a216427b5783e5d8894b2229c6b
jasonhorikawa <jasonhorikawa@gmail.com>
2026-05-21 20:58:29 -0700

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -89,11 +89,11 @@ async function sendFriendRequest(receiverId) {
     },
   ]);
 
-  if (error) {
-    alert("Friend request failed: " + error.message);
-  } else {
-    alert("Friend request sent!");
-  }
+  alert(
+  error
+    ? "Friend request failed: " + error.message
+    : "Friend request sent!"
+);
 }
 function compressImage(file, maxWidth = 1200, quality = 0.75) {
   return new Promise((resolve) => {
